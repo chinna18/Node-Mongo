aaaa8b24d094adc5317959a1037d0a4506b7bb0a
chinna18 <obulareddy570@gmail.com>
2021-07-06 10:35:15 +0530

    update

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const mongoClient = mongodb.MongoClient;
 //const dbUrl = "mongodb://127.0.0.1:27017"; //Mongo installed in system
 
 //Mongo Atlas cloud db url
-const dbUrl = "mongodb+srv://dbpuli:dbpuli@cluster0.eukkc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const dbUrl =
+  "mongodb+srv://dbpuli:dbpuli@cluster0.eukkc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 app.use(express.json()); //middleware to access body
 
@@ -27,26 +28,25 @@ app.post("/students", async (req, res) => {
   } catch (error) {
     res.json({ message: "Error occured" });
   } finally {
-    //close connection 
+    //close connection
     client.close();
   }
 });
 
 //get all the students
 app.get("/students", async (req, res) => {
-  //open connection
-  let client = await mongoClient.connect(dbUrl);
   try {
+    //open connection
+    let client = await mongoClient.connect(dbUrl);
     //select the db
     let db = client.db("studentsdb");
     //select the collections perform db operations
     const data = await db.collection("students").find().toArray();
     res.status(200).json({ message: "Success", data });
-  } catch (error) {
-    res.json({ message: "Error occured" });
-  } finally {
     //close connection
     client.close();
+  } catch (error) {
+    res.json({ message: "Error occured" });
   }
 });
 
@@ -54,23 +54,23 @@ app.get("/students", async (req, res) => {
 app.get("/students/:id", async (req, res) => {
   //open connection
   let client = await mongoClient.connect(dbUrl);
-  //converting ig to mongo db object id
+  //converting id to mongo db object id
   const objectId = mongodb.ObjectID(req.params.id);
   try {
     //select the db
     let db = client.db("studentsdb");
     //select the collections perform db operations
     const data = await db.collection("students").findOne({ _id: objectId });
-    if(data){
+    if (data) {
       res.status(200).json({ message: "Success", data });
-    } else{
-      res.json({message: "User not found with the given ID "})
+    } else {
+      res.json({ message: "User not found with the given ID " });
     }
   } catch (error) {
     res.json({ message: "Error occured" });
   } finally {
     //close connection
-    client.close(); 
+    client.close();
   }
 });
 
@@ -78,18 +78,20 @@ app.get("/students/:id", async (req, res) => {
 app.put("/student-update/:id", async (req, res) => {
   //open connection
   let client = await mongoClient.connect(dbUrl);
-  //converting ig to mongo db object id
+  //converting id to mongo db object id
   const objectId = mongodb.ObjectID(req.params.id);
   try {
     //selct the db
     let db = client.db("studentsdb");
     //select the collections perform db operations
     const data = await db.collection("students").findOne({ _id: objectId });
-    if(data){
-      const updated = await db.collection('students').findOneAndUpdate({_id: objectId },{$set:{age:req.body.age}})
-      res.json({message:"Record updated"})
-    }else{
-      res.json({message:'Record not found with given ID'})
+    if (data) {
+      const updated = await db
+        .collection("students")
+        .findOneAndUpdate({ _id: objectId }, { $set: { age: req.body.age } });
+      res.json({ message: "Record updated" });
+    } else {
+      res.json({ message: "Record not found with given ID" });
     }
   } catch (error) {
     console.log(error);
@@ -111,11 +113,13 @@ app.delete("/student-delete/:id", async (req, res) => {
     let db = client.db("studentsdb");
     //select the collections perform db operations
     const data = await db.collection("students").findOne({ _id: objectId });
-    if(data){
-      const deleted = await db.collection("students").findOneAndDelete({_id:objectId})
-      res.json({message:"Record deleted"})
-    }else{
-      res.json({message:'Record not found with given ID'})
+    if (data) {
+      const deleted = await db
+        .collection("students")
+        .findOneAndDelete({ _id: objectId });
+      res.json({ message: "Record deleted" });
+    } else {
+      res.json({ message: "Record not found with given ID" });
     }
   } catch (error) {
     console.log(error);
@@ -125,8 +129,6 @@ app.delete("/student-delete/:id", async (req, res) => {
     client.close();
   }
 });
-
-
 
 app.listen(port, () => {
   console.log("Server listening to port:" + port);
